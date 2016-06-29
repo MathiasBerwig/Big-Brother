389ed20f4ff1f6f91f6f2912acf76c0055af3246
Mathias Berwig <mathiasberwig@gmail.com>
2016-06-29 16:42:50 -0300

    Implementação da listagem de registros

---
+++ android/app/src/main/java/io/github/mathiasberwig/bigbrother/data/remote/RDAO.java
@@ -0,0 +1,61 @@
+package io.github.mathiasberwig.bigbrother.data.remote;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import io.github.mathiasberwig.bigbrother.data.model.Registro;
+
+public class RDAO {
+    private static final String TAG = "RDAO";
+
+    private static final String URL_SERVIDOR = "http://192.168.10.149/server/v1";
+    private static final String TEMPLATE_URL_GET_REGISTROS = "%s/getRegistros";
+    private static final String TEMPLATE_URL_GET_REGISTROS_TAG = "%s/getRegistros/%s";
+    private static final String TEMPLATE_URL_GET_REGISTROS_TAG_DT_HR = "%s/getRegistros/%s/%s/%s";
+
+    private static final SimpleDateFormat SERVER_DATETIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd+HH:mm:ss", Locale.getDefault());
+
+    private RequestQueue requestQueue;
+
+    private static RDAO instance;
+
+    private RDAO(Context context) {
+        requestQueue = Volley.newRequestQueue(context);
+    }
+
+    public static RDAO getInstance(Context context) {
+        if (instance == null) {
+            instance = new RDAO(context);
+        }
+        return instance;
+    }
+
+    public void getRegistros(Response.Listener<Registro[]> listener,
+                             Response.ErrorListener errorListener) {
+        final String URL = String.format(TEMPLATE_URL_GET_REGISTROS, URL_SERVIDOR);
+        requestQueue.add(new GsonRequest<>(URL, Registro[].class, null, listener, errorListener));
+    }
+
+    public void getRegistros(String tag, Response.Listener<Registro[]> listener,
+                             Response.ErrorListener errorListener) {
+        final String URL = String.format(TEMPLATE_URL_GET_REGISTROS_TAG, URL_SERVIDOR, tag);
+        requestQueue.add(new GsonRequest<>(URL, Registro[].class, null, listener, errorListener));
+    }
+
+    public void getRegistros(String tag, Date dataInicio, Date dataFim,
+                             Response.Listener<Registro[]> listener,
+                             Response.ErrorListener errorListener) {
+        String inicio = SERVER_DATETIME_FORMAT.format(dataInicio);
+        String fim = SERVER_DATETIME_FORMAT.format(dataFim);
+        final String URL = String.format(TEMPLATE_URL_GET_REGISTROS_TAG_DT_HR, URL_SERVIDOR, tag,
+                inicio, fim);
+        requestQueue.add(new GsonRequest<>(URL, Registro[].class, null, listener, errorListener));
+    }
+}
--- android/app/src/main/java/io/github/mathiasberwig/bigbrother/presentation/activity/MainActivity.java
+++ android/app/src/main/java/io/github/mathiasberwig/bigbrother/presentation/activity/MainActivity.java
@@ -1,15 +1,92 @@
 package io.github.mathiasberwig.bigbrother.presentation.activity;
 
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import java.util.Arrays;
 
 import io.github.mathiasberwig.bigbrother.R;
+import io.github.mathiasberwig.bigbrother.data.model.Registro;
+import io.github.mathiasberwig.bigbrother.data.remote.RDAO;
+import io.github.mathiasberwig.bigbrother.presentation.adapter.RegistrosAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private RecyclerView recyclerView;
+    private TextView tvEmptyState;
+    private RegistrosAdapter registrosAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        localizarViews();
+        prepararRecyclerView();
+        consultarRegistros();
+    }
+
+    private void prepararRecyclerView() {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+    }
+
+    private void localizarViews() {
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        tvEmptyState = (TextView) findViewById(R.id.tvEmptyState);
+    }
+
+    private void atualizarAdapter(Registro[] registros) {
+        if (registrosAdapter == null) {
+            registrosAdapter = new RegistrosAdapter(this, Arrays.asList(registros));
+            recyclerView.setAdapter(registrosAdapter);
+        } else {
+            registrosAdapter.setRegistros(Arrays.asList(registros));
+        }
+    }
+
+    private void mostrarEmptyState(@StringRes int mensagem) {
+        tvEmptyState.setText(mensagem);
+        recyclerView.setVisibility(View.GONE);
+        tvEmptyState.setVisibility(View.VISIBLE);
+    }
+
+    private void mostrarRecyclerView() {
+        tvEmptyState.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void consultarRegistros() {
+        RDAO.getInstance(this).getRegistros(
+                new Response.Listener<Registro[]>() {
+                    @Override
+                    public void onResponse(Registro[] response) {
+                        if (response.length > 0) {
+                            atualizarAdapter(response);
+                            mostrarRecyclerView();
+                        } else {
+                            mostrarEmptyState(R.string.erro_resposta_vazia);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mostrarEmptyState(R.string.erro_consultar_registros);
+                        Log.e(TAG, "consultarRegistros: " + error.getMessage(), error);
+                    }
+                }
+        );
     }
 }
